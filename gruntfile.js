a0a84e1c9ddadddc951f546c07e25d827edccb4d
IsaacFang <isaac.fang@grapecity.com>
2014-06-04 14:54:36 +0800

    modify files

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,49 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['client'],
+        copy: [],
+        cssmin: [],
+        jshint: {
+        },
+        less: {
+            compile: {
+                files: {
+                    'client/css/style.css': 'client/less/style.less'
+                }
+            }
+        },
+        uglify: {
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        watch: {
+            files: ['client/css/*.less'],
+            tasks: ['less']
+        },
+        githooks: {},
+        karma: {},
+        shell: {
+
+        }
 
     });
+
+    // Load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-githooks');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-shell');
+
+    // Register grunt tasks
+    grunt.registerTask('default', ['uglify']);
 };
