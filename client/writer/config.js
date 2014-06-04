988001b4ebb8ad4f0673bf0667a361f874ea4d3f
IsaacFang <isaac.fang@grapecity.com>
2014-06-04 22:14:47 +0800

    Changes to be committed: 	new file:   client/writer/app/app.js 	new file:   client/writer/app/controllers/CommentsController.js 	new file:   client/writer/app/controllers/PostsController.js 	new file:   client/writer/app/controllers/SettingsController.js 	new file:   client/writer/app/controllers/UsersController.js 	new file:   client/writer/app/controllers/controllers.js 	new file:   client/writer/app/controllers/homeController.js 	modified:   client/writer/config.js
     Changes not staged for commit:
    	modified:   bower.json
    	modified:   client/writer/app/app.js
    	modified:   client/writer/app/controllers/CommentsController.js
    	modified:   client/writer/app/controllers/PostsController.js
    	modified:   client/writer/app/controllers/SettingsController.js
    	modified:   client/writer/app/controllers/UsersController.js
    	modified:   client/writer/app/controllers/controllers.js
    	modified:   client/writer/app/controllers/homeController.js
    	modified:   client/writer/config.js
    	modified:   client/writer/index.html
    	modified:   client/writer/main.js
    	modified:   config.js
    	modified:   main.js
    	modified:   package.json

--- client/writer/config.js
+++ client/writer/config.js
@@ -2,3 +2,15 @@
  * Copyright 2006-2014 GrapeCity inc
  * Author: isaac.fang@grapecity.com
  */
+
+define(function () {
+    return {
+        appName: 'writer.oglen.net',
+        debugEnable: true,
+        languages: {
+            el: 'el',
+            zh: 'zh-CN',
+            en: 'en'
+        }
+    }
+});
